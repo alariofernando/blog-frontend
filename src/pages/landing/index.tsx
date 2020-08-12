bb25cdc10e0082bd20739906f569a234016caf2c
FERNANDO ALARIO PIRES <servicoiteris738113@userede.com.br>
2020-08-12 01:12:03 -0300

    Initial commit

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+    return(
+        <div id="page-landing">
+            <h2>Hello, world</h2>
+        </div>
+    )
+}
+
+export default Landing;
